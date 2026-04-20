20f844dd423e5b4aa6d0a29f56d4447b1fde4f8d
JuanCall <juancallerosales19@gmail.com>
2026-04-19 21:17:04 -0500

    Inicializacion/Publicacion de Sprints y tareas

--- caja-desktop/main.js
+++ caja-desktop/main.js
@@ -14,7 +14,7 @@ function createWindow () {
   });
 
   // Carga tu archivo HTML principal
-  mainWindow.loadFile(path.join(__dirname, 'src', 'caja.html'));
+  mainWindow.loadFile(path.join(__dirname, 'src', '.html'));
   
   mainWindow.webContents.openDevTools();
 }
